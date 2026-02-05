ccd980eb83717fe2797d4d8760da3cc210119da1
pavlekr <svemirac381@gmail.com>
2026-02-05 17:48:58 +0100

    dodane klase nedovrsene

---
+++ Covece ne ljuti se/Common/Igrac.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common
+{
+    public class Igrac
+    {
+        private string username;
+        private TipBoje boja;
+        private List<Pijun> pijuni;
+        int start;
+
+        public string Username
+        {
+            get { return username; }
+            set { username = value; }
+        }
+        public int Start
+        {
+            get { return start; }
+            set { start = value; }
+        }
+        public TipBoje Boja
+        {
+            get { return boja; }
+            set { boja = value; }
+        }
+        public List<Pijun> Pijuni
+        {
+            get { return pijuni; }
+            set { pijuni = value; }
+        }
+
+        public Igrac()
+        {
+            Username = string.Empty;
+            Boja = 0;
+            Pijuni = new List<Pijun>(4);
+            Start = 0;
+        }
+        public Igrac(string username, TipBoje boja, int start)
+        {
+            Username = username;
+            Boja = boja;
+            Start = start;
+            Pijuni = new List<Pijun>(4);
+
+            for(int i = 0; i < 4; i++)
+            {
+                Pijuni[i] = new Pijun(Start);
+            }
+        }
+    }
+
+    
+}
---
+++ Covece ne ljuti se/Common/Pijun.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common
+{
+    public class Pijun
+    {
+        private int pozicija;
+        private bool u_kucici;
+        private bool u_cilju;
+        private int do_cilja;
+
+        public int Pozicija
+        {
+            get {  return pozicija; }
+            set { pozicija = value; }
+        }
+        public bool U_kucici
+        {
+            get { return u_kucici; }
+            set { u_kucici = value; }
+        }
+        public bool U_cilju
+        {
+            get { return u_cilju; }
+            set { u_cilju = value; }
+        }
+        public int Do_cilja
+        {
+            get { return do_cilja; }
+            set { do_cilja = value; }
+        }
+
+        public Pijun()
+        {
+            Pozicija = -1;
+            U_kucici = true;
+            U_cilju = false;
+            Do_cilja = 0;
+        }
+
+        public Pijun(int br_polja)
+        {
+            Pozicija = -1;
+            Do_cilja = br_polja;
+            U_kucici = true;
+            U_cilju = false;
+        }
+
+        public override string ToString()
+        {
+            return $"Pijun: {Pozicija}, ({Do_cilja})";
+        }
+    }
+}
